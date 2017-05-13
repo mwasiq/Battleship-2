ae0fa49af6b1f31948db3c75f43a4c7f5f7d661e
mwasiq <100085735@students.swinburne.edu.my>
2017-05-13 20:27:25 +0800

    music set

---
+++ Battleships/src/GameLogic.cs
@@ -0,0 +1,49 @@
+
+using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+//using System.Data;
+using System.Diagnostics;
+using SwinGameSDK;
+
+static class GameLogic
+{
+	public static void Main()
+	{
+		//Opens a new Graphics Window
+		SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600); 
+
+		//Load Resources
+		GameResources.LoadResources();
+		//Game Loop
+
+
+		do
+		{
+
+			SwinGame.StopMusic ();
+			SwinGame.PlayMusic (GameResources.GameMusic (GameController.MusicOption)); 
+			GameController.Musicstate = false;
+
+			while (!( GameController.Musicstate == true | SwinGame.WindowCloseRequested () == true | GameController.CurrentState == GameState.Quitting) )
+			{
+				GameController.HandleUserInput ();
+				GameController.DrawScreen ();
+			} 
+	
+		} while (!(SwinGame.WindowCloseRequested () == true | GameController.CurrentState == GameState.Quitting));
+		SwinGame.StopMusic();
+	 
+
+		//Free Resources and Close Audio, to end the program.
+		GameResources.FreeResources();
+	}
+}
+
+//=======================================================
+//Service provided by Telerik (www.telerik.com)
+//Conversion powered by NRefactory.
+//Twitter: @telerik
+//Facebook: facebook.com/telerik
+//=======================================================
